fbb8574ff01979706e7a95d5a8f53fad5e6978da
selenaseelaa <qudsiyah.zahrain@gmail.com>
2023-09-23 10:38:49 +0700

    ini berita mi

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('sections.home');
 });
+
+Route::get('/berita', function () {
+    return view('sections.berita');
+});
